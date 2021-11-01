d2ce6f307621b8906e3a33ef0937f1ccd403a29a
SungFeng-Huang <f06942045@ntu.edu.tw>
2021-11-02 03:44:35 +0800

    Initialize meta-lingual framework

---
+++ lightning/model/phoneme_embedding.py
@@ -0,0 +1,86 @@
+import os
+import json
+import torch
+import pytorch_lightning as pl
+
+from torch import nn
+from torch.nn import functional as F
+
+from text.symbols import symbols
+from transformer import Encoder, Decoder, PostNet, Constants
+
+
+class PhonemeEmbedding(pl.LightningModule):
+
+    def __new__(cls, emb_type, model_config, default_emb=None):
+        if emb_type == 'mono-lingual' or emb_type == 'multi-lingual':
+            # If already constructed, use the old one, which means nothing
+            # happened.
+            if default_emb is not None:
+                return default_emb
+        return object.__new__(cls, emb_type, model_config, default_emb)
+
+
+    def __init__(self, emb_type, model_config, default_emb=None):
+        """
+        All the data members would be moved to cuda together with the whole model
+        by pytorch_lightning default settings.
+        Directly replacing `model.encoder.src_word_emb` with this class would do
+        no harm to the `model.encoder` forwarding.
+        """
+        super().__init__()
+
+        self.emb_type = emb_type
+        n_src_vocab = len(symbols) + 1
+        d_word_vec = modle_config["transformer"]["encoder_hidden"]
+
+        if emb_type == 'mono-lingual' or emb_type == 'multi-lingual':
+            self.src_word_emb = nn.Embedding(
+                n_src_vocab, d_word_vec, padding_idx=Constants.PAD
+            )
+
+        elif emb_type == 'meta-lingual':
+            codebook_size = model_config["codebook_size"]
+            d_feat = config["representation_dim"]
+            self.banks = nn.Embedding(
+                codebook_size, d_feat, padding_idx=Constants.PAD
+            )
+            self.proj = nn.Linear(d_feat, d_word_vec)
+            self.weighting_matrix = torch.zeros(n_src_vocab, codebook_size, requires_grad=False)
+
+
+    @torch.no_grad()
+    def set_quantize_matrix(self, ref):
+        """ Compute binary quantize matrix from reference representations.
+        Should run before inner_loop update.
+
+        Args:
+            ref: Reference representations with size (vocab_size, codebook_size),
+                 where vocab_size <= n_src_vocab.
+                 Assert the tensor is already moved to cuda by pytorch_lightning.
+        """
+        # NOTE: make sure padding_idx of self.weighting_matrix is correct
+        ref_norm = ref / ref.norm(dim=1, keepdim=True)
+        banks_norm = self.banks / self.banks.norm(dim=1, keepdim=True)
+        similarity = ref_norm @ banks_norm.T
+        # NOTE: why create tensor again, probably not need to transfer to cuda again
+        # similarity = dot_product_similarity(
+            # torch.tensor(ref, dtype=torch.float32).cuda(), self.banks.T
+        # )  # (vocab_size, codebook_size)
+
+        self.weighting_matrix.zero_()
+        self.weighting_matrix[torch.arange(len(similarity)), similarity.argmax(1)] = 1
+
+
+    def forward(self, args):
+        src_seq = args  # shape: (batch_size, max_len)
+
+        if self.emb_type == 'mono-lingual' or self.emb_type == 'multi-lingual':
+            return self.src_word_emb(src_seq)
+
+        elif self.emb_type == 'meta-lingual':
+            weighted_embedding = self.weighting_matrix @ self.banks
+            output = F.embedding(src_seq, weighted_embedding, padding_idx=Constants.PAD)
+            output = self.proj(output)
+            return output
+
--- lightning/systems/meta.py
+++ lightning/systems/meta.py
@@ -21,6 +21,30 @@ class MetaSystem(BaseAdaptorSystem):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def _on_meta_batch_start(self, batch):
+        """ Check meta-batch data """
+        # NOTE: should rename `ref_p_embedding` to something like
+        # `ref_phn_feats` or `ref_phn_representations`
+        assert len(batch) == 1, "meta_batch_per_gpu"
+        assert len(batch[0]) == 3, "sup + qry + ref_p_embedding"
+        assert len(batch[0][0]) == 1, "n_batch == 1"
+        assert len(batch[0][0][0]) == 12, "data with 12 elements"
+
+    def on_after_batch_transfer(self, batch, dataloader_idx):
+        # NOTE: `self.model.encoder` and `self.learner.encoder` are pointing to
+        # the same variable, they are not two variables with the same values.
+        ref_phn_feats = batch[0][2]
+        self.model.encoder.src_word_emb.set_quantize_matrix(ref_phn_feats)
+        del batch[0][2]
+        return batch
+
+    # Second order gradients for RNNs
+    @torch.backends.cudnn.flags(enabled=False)
+    @torch.enable_grad()
+    def adapt(self, batch, adaptation_steps=5, learner=None, train=True):
+        # TODO: overwrite for supporting SGD and iMAML
+        return super().adapt(batch, adaptation_steps, learner, train)
+
     def on_train_batch_start(self, batch, batch_idx, dataloader_idx):
         self._on_meta_batch_start(batch)
 
@@ -34,7 +58,7 @@ class MetaSystem(BaseAdaptorSystem):
         qry_batch = batch[0][1][0]
 
         # Log metrics to CometLogger
-        loss_dict = {f"Train/{k}":v for k,v in loss2dict(train_loss).items()}
+        loss_dict = {f"Train/{k}": v for k, v in loss2dict(train_loss).items()}
         self.log_dict(loss_dict, sync_dist=True)
         return {'loss': train_loss[0], 'losses': train_loss, 'output': predictions, '_batch': qry_batch}
 
@@ -51,7 +75,7 @@ class MetaSystem(BaseAdaptorSystem):
         qry_batch = batch[0][1][0]
 
         # Log metrics to CometLogger
-        loss_dict = {f"Val/{k}":v for k,v in loss2dict(val_loss).items()}
+        loss_dict = {f"Val/{k}": v for k, v in loss2dict(val_loss).items()}
         self.log_dict(loss_dict, sync_dist=True)
         return {'losses': val_loss, 'output': predictions, '_batch': qry_batch}
 
