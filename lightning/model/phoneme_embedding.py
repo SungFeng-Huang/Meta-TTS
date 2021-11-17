ff742ca9798c45e9cb23f664013316076c5dc17b
SungFeng-Huang <f06942045@ntu.edu.tw>
2021-11-18 02:03:32 +0800

    First attemp for meta-codebook

--- lightning/model/phoneme_embedding.py
+++ lightning/model/phoneme_embedding.py
@@ -1,27 +1,41 @@
 import os
 import json
-import torch
-import pytorch_lightning as pl
+import numpy as np
 
+import torch
 from torch import nn
 from torch.nn import functional as F
 
+import pytorch_lightning as pl
+
+import transformer.Constants as Constants
+from transformer import Encoder, Decoder, PostNet
+from transformer.Modules import ScaledDotProductAttention
 from text.symbols import symbols
-from transformer import Encoder, Decoder, PostNet, Constants
+
+
+N_SRC_VOCAB = len(symbols) + 1
 
 
 class PhonemeEmbedding(pl.LightningModule):
 
-    def __new__(cls, emb_type, model_config, default_emb=None):
-        if emb_type == 'mono-lingual' or emb_type == 'multi-lingual':
-            # If already constructed, use the old one, which means nothing
-            # happened.
-            if default_emb is not None:
-                return default_emb
-        return object.__new__(cls, emb_type, model_config, default_emb)
+    def __new__(cls, model_config, algorithm_config):
+        emb_type = algorithm_config["adapt"]["phoneme_emb"]["type"]
+
+        # If not using codebook, return nn.Embedding instead.
+        if emb_type == 'embedding':
+            # NOTE: currently just use multi-lingual vocab_size
+            # TODO: mono-lingual
+            d_word_vec = model_config["transformer"]["encoder_hidden"]
+
+            return nn.Embedding(
+                N_SRC_VOCAB, d_word_vec, padding_idx=Constants.PAD
+            )
+
+        return object.__new__(cls)
 
 
-    def __init__(self, emb_type, model_config, default_emb=None):
+    def __init__(self, model_config, algorithm_config):
         """
         All the data members would be moved to cuda together with the whole model
         by pytorch_lightning default settings.
@@ -29,58 +43,87 @@ class PhonemeEmbedding(pl.LightningModule):
         no harm to the `model.encoder` forwarding.
         """
         super().__init__()
+        assert algorithm_config["adapt"]["phoneme_emb"]["type"] == 'codebook'
 
-        self.emb_type = emb_type
-        n_src_vocab = len(symbols) + 1
-        d_word_vec = modle_config["transformer"]["encoder_hidden"]
+        # NOTE: currently just use multi-lingual vocab_size
+        # TODO: mono-lingual
+        d_word_vec = model_config["transformer"]["encoder_hidden"]
 
-        if emb_type == 'mono-lingual' or emb_type == 'multi-lingual':
-            self.src_word_emb = nn.Embedding(
-                n_src_vocab, d_word_vec, padding_idx=Constants.PAD
+        self.codebook_config = algorithm_config["adapt"]["phoneme_emb"]["codebook"]
+        codebook_size = self.codebook_config["size"]
+
+        self.emb_banks = nn.Embedding(
+            codebook_size, d_word_vec, padding_idx=Constants.PAD
+        )
+
+        self.share_att_banks = self.codebook_config["attention"]["share_banks"]
+        if self.share_att_banks:
+            # feats -> proj -> att(att_banks) -> token_id -> emb_banks
+            self.att_banks = nn.Embedding(
+                codebook_size, d_word_vec, padding_idx=Constants.PAD
             )
-
-        elif emb_type == 'meta-lingual':
-            codebook_size = model_config["codebook_size"]
-            d_feat = config["representation_dim"]
-            self.banks = nn.Embedding(
+            # TPU shared weights are copied independently
+            # on the XLA device and this line won't have any effect.
+            # However, it works fine for CPU and GPU.
+            self.att_banks.weight = self.emb_banks.weight
+        else:
+            # feats -> att(att_banks) -> token_id -> emb_banks
+            d_feat = self.codebook_config["representation_dim"]
+            self.att_banks = nn.Embedding(
                 codebook_size, d_feat, padding_idx=Constants.PAD
             )
-            self.proj = nn.Linear(d_feat, d_word_vec)
-            self.weighting_matrix = torch.zeros(n_src_vocab, codebook_size, requires_grad=False)
+
+        if self.codebook_config["attention"]["name"] == "cos_sim_argmax":
+            self.weighting_matrix = torch.zeros(N_SRC_VOCAB, codebook_size, requires_grad=False)
+        elif self.codebook_config["attention"]["name"] == "scaled_dot_product_att":
+            self.attention = ScaledDotProductAttention(temperature=np.power(d_k, 0.5))
 
 
-    @torch.no_grad()
-    def set_quantize_matrix(self, ref):
+    def on_post_move_to_device(self):
+        if getattr(self, 'share_att_banks', False):
+            # Weights shared after the model has been moved to TPU Device
+            self.att_banks.weight = self.emb_banks.weight
+
+
+    def set_quantize_matrix(self, ref, proj_layer=None):
         """ Compute binary quantize matrix from reference representations.
         Should run before inner_loop update.
 
         Args:
             ref: Reference representations with size (vocab_size, codebook_size),
-                 where vocab_size <= n_src_vocab.
+                 where vocab_size <= N_SRC_VOCAB.
                  Assert the tensor is already moved to cuda by pytorch_lightning.
         """
-        # NOTE: make sure padding_idx of self.weighting_matrix is correct
-        ref_norm = ref / ref.norm(dim=1, keepdim=True)
-        banks_norm = self.banks / self.banks.norm(dim=1, keepdim=True)
-        similarity = ref_norm @ banks_norm.T
-        # NOTE: why create tensor again, probably not need to transfer to cuda again
-        # similarity = dot_product_similarity(
-            # torch.tensor(ref, dtype=torch.float32).cuda(), self.banks.T
-        # )  # (vocab_size, codebook_size)
+        try:
+            assert ref.device == self.device
+        except:
+            ref = ref.to(device=self.device)
+        if getattr(self, 'share_att_banks', False) and proj_layer is not None:
+            ref = proj_layer(ref)
 
-        self.weighting_matrix.zero_()
-        self.weighting_matrix[torch.arange(len(similarity)), similarity.argmax(1)] = 1
+        if self.codebook_config["attention"]["name"] == "cos_sim_argmax":
+            # NOTE: make sure padding_idx of self.weighting_matrix is correct
+            ref_norm = ref / ref.norm(dim=1, keepdim=True)
+            banks_norm = self.att_banks / self.att_banks.norm(dim=1, keepdim=True)
+            similarity = ref_norm @ banks_norm.T
+
+            with torch.no_grad():
+                self.weighting_matrix.zero_()
+                self.weighting_matrix[torch.arange(len(similarity)), similarity.argmax(1)] = 1
+
+        elif self.codebook_config["attention"]["name"] == "scaled_dot_product_att":
+            """ Soft attention weight. Better not share_att_banks.
+            key: self.att_banks
+            value: self.emb_banks
+            query: ref
+            """
 
 
     def forward(self, args):
         src_seq = args  # shape: (batch_size, max_len)
 
-        if self.emb_type == 'mono-lingual' or self.emb_type == 'multi-lingual':
-            return self.src_word_emb(src_seq)
-
-        elif self.emb_type == 'meta-lingual':
-            weighted_embedding = self.weighting_matrix @ self.banks
-            output = F.embedding(src_seq, weighted_embedding, padding_idx=Constants.PAD)
-            output = self.proj(output)
-            return output
+        weighted_embedding = self.weighting_matrix @ self.banks
+        output = F.embedding(src_seq, weighted_embedding, padding_idx=Constants.PAD)
+        output = self.proj(output)
+        return output
 
