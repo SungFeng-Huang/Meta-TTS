83da5cec5ae4392852180ab83bf836e94bf444a3
SungFeng-Huang <f06942045@ntu.edu.tw>
2021-12-27 16:05:25 +0800

    Add train-clean-360 and train-other-500

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import argparse
-
+import shutil
 import yaml
+import os
 
 from preprocessor.preprocessor import Preprocessor
 
@@ -11,5 +12,25 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
+    if "LibriTTS" in config["dataset"]:
+        if config["subsets"]["train"] == "train-clean":
+            config["subsets"]["train"] = ["train-clean-100", "train-clean-360"]
+        elif config["subsets"]["train"] == "train-all":
+            config["subsets"]["train"] = ["train-clean-100", "train-clean-360", "train-other-500"]
     preprocessor = Preprocessor(config)
     preprocessor.build_from_path()
+    if "LibriTTS" in config["dataset"]:
+        if config["subsets"]["train"] == ["train-clean-100", "train-clean-360"]:
+            with open(os.path.join(config["path"]["preprocessed_path"], "train-clean.txt"), 'wb') as out_file:
+                with open(os.path.join(config["path"]["preprocessed_path"], "train-clean-100.txt"), 'rb') as in_file:
+                    shutil.copyfileobj(in_file, out_file)
+                with open(os.path.join(config["path"]["preprocessed_path"], "train-clean-360.txt"), 'rb') as in_file:
+                    shutil.copyfileobj(in_file, out_file)
+        elif config["subsets"]["train"] == ["train-clean-100", "train-clean-360", "train-other-500"]:
+            with open(os.path.join(config["path"]["preprocessed_path"], "train-all.txt"), 'wb') as out_file:
+                with open(os.path.join(config["path"]["preprocessed_path"], "train-clean-100.txt"), 'rb') as in_file:
+                    shutil.copyfileobj(in_file, out_file)
+                with open(os.path.join(config["path"]["preprocessed_path"], "train-clean-360.txt"), 'rb') as in_file:
+                    shutil.copyfileobj(in_file, out_file)
+                with open(os.path.join(config["path"]["preprocessed_path"], "train-other-500.txt"), 'rb') as in_file:
+                    shutil.copyfileobj(in_file, out_file)
