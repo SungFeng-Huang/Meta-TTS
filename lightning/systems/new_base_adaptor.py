cf174b70b02900f1a72ecdf35546c393193de93d
SungFeng-Huang <f06942045@ntu.edu.tw>
2022-03-21 06:08:08 +0800

    Add custom config to reference_prosody and d_targets

--- lightning/systems/new_base_adaptor.py
+++ lightning/systems/new_base_adaptor.py
@@ -37,6 +37,9 @@ class BaseAdaptorSystem(System):
         self.test_adaptation_steps = self.algorithm_config["adapt"]["test"]["steps"]
         assert self.test_adaptation_steps % self.adaptation_steps == 0
 
+        self.d_target = False
+        self.reference_prosody = True
+
     # Second order gradients for RNNs
     @torch.backends.cudnn.flags(enabled=False)
     @torch.enable_grad()
@@ -50,25 +53,36 @@ class BaseAdaptorSystem(System):
 
         # Adapt the classifier
         sup_batch = batch[0][0][0]
+        reference_prosody = sup_batch[9].unsqueeze(2) if self.reference_prosody else None
         first_order = not train
         for step in range(adaptation_steps):
-            preds = learner(*sup_batch[2:], reference_prosody=sup_batch[9].unsqueeze(2))
+            preds = learner(*sup_batch[2:], reference_prosody=reference_prosody)
             train_error = self.loss_func(sup_batch, preds)
             learner.adapt_(train_error[0], first_order=first_order, allow_unused=False, allow_nograd=True)
         return learner
 
     def meta_learn(self, batch, batch_idx, train=True):
         learner = self.adapt(batch, min(self.adaptation_steps, self.test_adaptation_steps), train=train)
-        learner.module.variance_adaptor.eval()
+        if train:
+            learner.module.variance_adaptor.eval()
+        else:
+            learner.eval()
 
         sup_batch = batch[0][0][0]
         qry_batch = batch[0][1][0]
+        reference_prosody = qry_batch[9].unsqueeze(2) if self.reference_prosody else None
 
         # Evaluating the adapted model
         # Use speaker embedding of support set
-        predictions = learner(sup_batch[2], *qry_batch[3:],
-                              average_spk_emb=True,
-                              reference_prosody=qry_batch[9].unsqueeze(2))
+        if self.d_target:
+            predictions = learner(sup_batch[2], *qry_batch[3:9],
+                                  d_targets=qry_batch[11],
+                                  average_spk_emb=True,
+                                  reference_prosody=reference_prosody)
+        else:
+            predictions = learner(sup_batch[2], *qry_batch[3:],
+                                  average_spk_emb=True,
+                                  reference_prosody=reference_prosody)
         valid_error = self.loss_func(qry_batch, predictions)
         return valid_error, predictions
 
@@ -108,6 +122,7 @@ class BaseAdaptorSystem(System):
 
         sup_batch = batch[0][0][0]
         qry_batch = batch[0][1][0]
+        reference_prosody = qry_batch[9].unsqueeze(2) if self.reference_prosody else None
         outputs['_batch'] = qry_batch
 
         # Evaluating the initial model
@@ -118,9 +133,15 @@ class BaseAdaptorSystem(System):
         outputs[f"step_0"] = {"recon": {"losses": valid_error, "output": predictions}}
 
         # synth_samples & save & log
-        predictions = self.learner(sup_batch[2], *qry_batch[3:6],
-                                   average_spk_emb=True,
-                                   reference_prosody=qry_batch[9].unsqueeze(2))
+        if self.d_target:
+            predictions = self.learner(sup_batch[2], *qry_batch[3:9],
+                                       d_targets=qry_batch[11],
+                                       average_spk_emb=True,
+                                       reference_prosody=reference_prosody)
+        else:
+            predictions = self.learner(sup_batch[2], *qry_batch[3:6],
+                                       average_spk_emb=True,
+                                       reference_prosody=reference_prosody)
         outputs[f"step_0"].update({"synth": {"output": predictions}})
 
         # Adapt
@@ -130,17 +151,29 @@ class BaseAdaptorSystem(System):
 
             # Evaluating the adapted model
             learner.eval()
-            predictions = learner(sup_batch[2], *qry_batch[3:],
-                                  average_spk_emb=True,
-                                  reference_prosody=qry_batch[9].unsqueeze(2))
+            if self.d_target:
+                predictions = learner(sup_batch[2], *qry_batch[3:9],
+                                      d_targets=qry_batch[11],
+                                      average_spk_emb=True,
+                                      reference_prosody=reference_prosody)
+            else:
+                predictions = learner(sup_batch[2], *qry_batch[3:],
+                                      average_spk_emb=True,
+                                      reference_prosody=reference_prosody)
             valid_error = self.loss_func(qry_batch, predictions)
             outputs[f"step_{ft_step}"] = {"recon": {"losses": valid_error, "output": predictions}}
 
             if ft_step in saving_steps:
                 # synth_samples & save & log
-                predictions = learner(sup_batch[2], *qry_batch[3:6],
-                                      average_spk_emb=True,
-                                      reference_prosody=qry_batch[9].unsqueeze(2))
+                if self.d_target:
+                    predictions = learner(sup_batch[2], *qry_batch[3:9],
+                                          d_targets=qry_batch[11],
+                                          average_spk_emb=True,
+                                          reference_prosody=reference_prosody)
+                else:
+                    predictions = learner(sup_batch[2], *qry_batch[3:6],
+                                          average_spk_emb=True,
+                                          reference_prosody=reference_prosody)
                 outputs[f"step_{ft_step}"].update({"synth": {"output": predictions}})
         del learner
 
