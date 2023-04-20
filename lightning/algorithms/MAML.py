3f81115602f96c69f29e5d894ebb7c74f7aaf937
SungFeng-Huang <f06942045@ntu.edu.tw>
2023-04-20 18:09:43 +0800

    Fix AdamWMAML wit unit test

--- lightning/algorithms/MAML.py
+++ lightning/algorithms/MAML.py
@@ -115,7 +115,7 @@ class AlphaMAML(GBML):
         )
 
 
-class AdaMAML(GBML):
+class AdamWMAML(GBML):
     def __init__(
         self,
         module,
@@ -127,6 +127,7 @@ class AdaMAML(GBML):
         betas: Tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-8,
         weight_decay: float = 0.0,
+        correct_bias: bool = True,
         **kwargs
     ):
         if lr < 0.0:
@@ -138,19 +139,20 @@ class AdaMAML(GBML):
         if not 0.0 <= eps:
             raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
 
+        assert correct_bias
         alpha = kwargs.get("alpha", 1.0)
-        defaults = dict(betas=betas, eps=eps, correct_bias=True)
+        defaults = dict(betas=betas, eps=eps, correct_bias=correct_bias)
 
         # Customized compute_update for weight_decay
-        adam_transform = functools.partial(AdamTransform, alpha=alpha, defaults=defaults)
-        compute_update = WeightDecayParameterUpdate(
+        adamw_transform = functools.partial(AdamWTransform, alpha=alpha, defaults=defaults)
+        compute_update = AdvancedParameterUpdate(
             parameters=module.parameters(),
-            transform=ModuleTransform(adam_transform),
+            transform=ModuleTransform(adamw_transform),
             weight_decay=weight_decay,
         )
         super().__init__(
             module,
-            adam_transform,
+            adamw_transform,
             lr=lr,
             adapt_transform=adapt_transform,
             first_order=first_order,
@@ -177,13 +179,22 @@ class AdaMAML(GBML):
             allow_nograd=allow_nograd,
         )
 
-class WeightDecayParameterUpdate(ParameterUpdate):
-    def __init__(self, parameters, transform, weight_decay: float = 0.0):
+class AdvancedParameterUpdate(ParameterUpdate):
+    def __init__(
+        self,
+        parameters,
+        transform,
+        weight_decay: float = 0.0,
+        clip_grad_norm: float = 0.0,
+    ):
         """
+        Advanced ParameterUpdate with WD and clip_grad_norm available.
+
         Arguments:
             parameters (list): Parameters of the model to update.
             transform (Callable): A callable that returns an instantiated transform given a parameter.
             weight_decay (float): Weight decay factor for AdamW.
+            clip_grad_norm (float): If > 0, perform clip_grad_norm_() before Adam optimization.
 
         Modules:
             transforms_modules (ModuleList): List of transforms per parameter
@@ -192,7 +203,10 @@ class WeightDecayParameterUpdate(ParameterUpdate):
             transforms_indeces (list): List of transform ids (or None for non-transforms).
         """
         super().__init__(parameters=parameters, transform=transform)
-        self.weight_decay = weight_decay
+        # self.weight_decay = weight_decay
+        # self.clip_grad_norm = clip_grad_norm
+        self.register_buffer("weight_decay", torch.tensor(weight_decay))
+        self.register_buffer("clip_grad_norm", torch.tensor(clip_grad_norm))
 
     def forward(
         self,
@@ -256,16 +270,17 @@ class WeightDecayParameterUpdate(ParameterUpdate):
                       'allow_unused=True ?'
                 print(msg)
 
-        updates = []
-        grad_clip_norm = 1
-        # with torch.no_grad():
-        total_norm = torch.norm(torch.stack([torch.norm(g, 2.0) for g in gradients if g is not None]), 2.0)
-        clip_coef = grad_clip_norm / (total_norm + 1e-6)
-        clip_coef_clamped = torch.clamp(clip_coef, max=1.0)
-        for g in gradients:
-            if g is not None:
-                g = g * clip_coef_clamped
-            updates.append(g)
+        if self.clip_grad_norm > 0:
+            updates = []
+            total_norm = torch.norm(torch.stack([torch.norm(g, 2.0) for g in gradients if g is not None]), 2.0)
+            clip_coef = self.clip_grad_norm / (total_norm + 1e-6)
+            clip_coef_clamped = torch.clamp(clip_coef, max=1.0)
+            for g in gradients:
+                if g is not None:
+                    g = g * clip_coef_clamped
+                updates.append(g)
+        else:
+            updates = gradients
 
         wd_updates = []
         for p, g, t in zip(list(parameters), updates, self.transforms_indices):
@@ -283,7 +298,7 @@ class WeightDecayParameterUpdate(ParameterUpdate):
 
 
 
-class AdamTransform(Scale):
+class AdamWTransform(Scale):
     """
     ElementWiseScale per parameter for Adam operation
     """
@@ -316,9 +331,7 @@ class AdamTransform(Scale):
         #     "exp_avg": torch.zeros(*shape),
         #     "exp_avg_sq": torch.zeros(*shape),
         # }
-        self.step = 0
-        # self.exp_avg = self.register_buffer("exp_avg", torch.zeros(shape[0]))
-        # self.exp_avg_sq = torch.zeros(shape[0])
+        self.register_buffer("step", torch.tensor(0.))
         self.register_buffer("exp_avg", torch.zeros(shape[0]))
         self.register_buffer("exp_avg_sq", torch.zeros(shape[0]))
 
@@ -328,9 +341,6 @@ class AdamTransform(Scale):
 
         step = self.step + 1
 
-        # exp_avg = torch.add(torch.mul(exp_avg, beta1), grad, alpha=1.0 - beta1)
-        # exp_avg_sq = torch.addcmul(torch.mul(exp_avg_sq, beta2), grad, grad, value=1.0 - beta2)
-        # denom = exp_avg_sq.sqrt().add_(group["eps"])
         exp_avg = beta1 * exp_avg + (1.0 - beta1) * grad
         exp_avg_sq = beta2 * exp_avg_sq + (1.0 - beta2) * grad * grad
         denom = torch.sqrt(exp_avg_sq) + self.defaults["eps"]
@@ -341,121 +351,10 @@ class AdamTransform(Scale):
             bias_correction2 = 1.0 - beta2 ** step
             step_size = step_size * torch.sqrt(bias_correction2) / bias_correction1
 
-        # p.data.addcdiv_(exp_avg, denom, value=-step_size)
-        update = grad + step_size * exp_avg / denom
+        update = step_size * exp_avg / denom
 
         self.exp_avg = exp_avg
         self.exp_avg_sq = exp_avg_sq
         self.step = step
 
         return update * self.alpha
-
-
-# class AdamW(Optimizer):
-#     """
-#     Implements Adam algorithm with weight decay fix as introduced in
-#     `Decoupled Weight Decay Regularization <https://arxiv.org/abs/1711.05101>`__.
-#     Parameters:
-#         params (:obj:`Iterable[torch.nn.parameter.Parameter]`):
-#             Iterable of parameters to optimize or dictionaries defining parameter groups.
-#         lr (:obj:`float`, `optional`, defaults to 1e-3):
-#             The learning rate to use.
-#         betas (:obj:`Tuple[float,float]`, `optional`, defaults to (0.9, 0.999)):
-#             Adam's betas parameters (b1, b2).
-#         eps (:obj:`float`, `optional`, defaults to 1e-6):
-#             Adam's epsilon for numerical stability.
-#         weight_decay (:obj:`float`, `optional`, defaults to 0):
-#             Decoupled weight decay to apply.
-#         correct_bias (:obj:`bool`, `optional`, defaults to `True`):
-#             Whether ot not to correct bias in Adam (for instance, in Bert TF repository they use :obj:`False`).
-#     """
-#     def __init__(
-#         self,
-#         params: Iterable[torch.nn.parameter.Parameter],
-#         lr: float = 1e-3,
-#         betas: Tuple[float, float] = (0.9, 0.999),
-#         eps: float = 1e-7,
-#         weight_decay: float = 0.0,
-#         correct_bias: bool = True,
-#     ):
-#         if lr < 0.0:
-#             raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
-#         if not 0.0 <= betas[0] < 1.0:
-#             raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
-#         if not 0.0 <= betas[1] < 1.0:
-#             raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
-#         if not 0.0 <= eps:
-#             raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
-#         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
-#         super().__init__(params, defaults)
-
-#     def step(self, closure: Callable = None):
-#         """
-#         Performs a single optimization step.
-#         Arguments:
-#             closure (:obj:`Callable`, `optional`): A closure that reevaluates the model and returns the loss.
-#         """
-#         loss = None
-#         if closure is not None:
-#             loss = closure()
-
-#         for group in self.param_groups:
-#             for p in group["params"]:
-#                 if p.grad is None:
-#                     continue
-#                 grad = p.grad.data
-#                 if grad.is_sparse:
-#                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-#                 state = self.state[p]
-
-#                 # State initialization
-#                 if len(state) == 0:
-#                     state["step"] = 0
-#                     # Exponential moving average of gradient values
-#                     state["exp_avg"] = torch.zeros_like(p.data)
-#                     # Exponential moving average of squared gradient values
-#                     state["exp_avg_sq"] = torch.zeros_like(p.data)
-
-#                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-#                 beta1, beta2 = group["betas"]
-
-#                 state["step"] += 1
-
-#                 # Decay the first and second moment running average coefficient
-#                 # In-place operations to update the averages at the same time
-#                 exp_avg.mul_(beta1).add_(grad, alpha=1.0 - beta1)
-#                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
-#                 denom = exp_avg_sq.sqrt().add_(group["eps"])
-
-#                 step_size = group["lr"]
-#                 if group["correct_bias"]:  # No bias correction for Bert
-#                     bias_correction1 = 1.0 - beta1 ** state["step"]
-#                     bias_correction2 = 1.0 - beta2 ** state["step"]
-#                     step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
-
-#                 p.data.addcdiv_(exp_avg, denom, value=-step_size)
-
-#                 # Just adding the square of the weights to the loss function is *not*
-#                 # the correct way of using L2 regularization/weight decay with Adam,
-#                 # since that will interact with the m and v parameters in strange ways.
-#                 #
-#                 # Instead we want to decay the weights in a manner that doesn't interact
-#                 # with the m/v parameters. This is equivalent to adding the square
-#                 # of the weights to the loss with plain (non-momentum) SGD.
-#                 # Add weight decay at the end (fixed version)
-#                 if group["weight_decay"] > 0.0:
-#                     p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
-
-#         return loss
-        
-#     def get_lr(self):
-#         lr = []
-#         for group in self.param_groups:
-#             for p in group['params']:
-#                 state = self.state[p]
-#                 if len(state) == 0:
-#                     pass
-#                 else:
-#                     lr.append(group['lr'])
-#         return lr
---
+++ tests/test_adam_maml.py
@@ -0,0 +1,91 @@
+import sys
+import os
+import torch
+import pytest
+
+from torch.testing import assert_allclose, assert_close
+from collections import OrderedDict
+
+from torch.utils.data import DataLoader, Dataset
+
+from pytorch_lightning import LightningModule, Trainer
+
+class RandomDataset(Dataset):
+    def __init__(self, size, num_samples):
+        self.len = num_samples
+        self.data = torch.randn(num_samples, size)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.len
+
+num_samples = 10000
+
+class BoringModel(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.layer = torch.nn.Linear(32, 2)
+
+    def forward(self, x):
+        output = self.layer(x)
+        # self.print(self.trainer.state.stage)
+        # self.print(self.layer.weight)
+        # self.print(self.layer.bias)
+        # self.print()
+        print(self.layer.weight)
+        print(self.layer.bias)
+        print()
+        return output
+
+    def training_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("train_loss", loss)
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("valid_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("test_loss", loss)
+
+    def configure_optimizers(self):
+        # return torch.optim.Adam(self.parameters(), lr=0.1, weight_decay=0.1)
+        return torch.optim.AdamW(self.parameters(), lr=0.1, weight_decay=0.1)
+    
+
+def test_adam():
+    model_0 = BoringModel()
+    model_1 = BoringModel()
+    model_1.load_state_dict(model_0.state_dict())
+
+    from lightning.algorithms.MAML import AdamWMAML
+    adam_0 = model_0.configure_optimizers()
+    adam_1 = AdamWMAML(model_1, lr=0.1, first_order=True, weight_decay=0.1)
+
+    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+
+    batch = list(train_data)[0]
+    output_0 = model_0.training_step(batch, 0)
+    output_1 = model_1.training_step(batch, 0)
+    assert_close(output_1['loss'], output_0['loss'])
+
+    adam_0.zero_grad()
+    output_0['loss'].backward()
+    adam_0.step()
+    adam_1.adapt_(loss=output_1['loss'])
+
+    state_dict_0 = model_0.state_dict()
+    state_dict_1 = adam_1.module.state_dict()
+    for n in state_dict_0:
+        assert_close(state_dict_1[n], state_dict_0[n])
+
+    adam_0_states = adam_0.state_dict()['state']
+    for i in adam_0_states:
+        adam_0_state = adam_0_states[i]
+        adam_1_state = adam_1.compute_update.transforms_modules[i].transform.state_dict()
+        for k in adam_0_state:
+            assert_close(adam_1_state[k].view(-1), adam_0_state[k].view(-1))
